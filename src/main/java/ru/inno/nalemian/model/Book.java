6905d253ee9e745d97ed92b4ec03aa316a7cce30
nalemian <adelianagimova@gmail.com>
2025-04-09 23:30:58 +0300

    БД и Spring: создала сущность для таблицы, в контроллере имплементировала CRUD-операции

---
+++ src/main/java/ru/inno/nalemian/model/Book.java
@@ -0,0 +1,44 @@
+package ru.inno.nalemian.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private BigDecimal price;
+    private LocalDate publishDate;
+
+    public Book() {
+    }
+
+    public Book(String title, BigDecimal price, LocalDate publishDate) {
+        this.title = title;
+        this.price = price;
+        this.publishDate = publishDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public LocalDate getPublishDate() {
+        return publishDate;
+    }
+}
