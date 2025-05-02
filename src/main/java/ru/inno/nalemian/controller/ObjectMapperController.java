077113f3357c8b208c6e17562b158a39b3aab38e
nalemian <adelianagimova@gmail.com>
2025-05-02 18:07:36 +0300

    возврат и приём DTO с форматированной датой

--- src/main/java/ru/inno/nalemian/controller/ObjectMapperController.java
+++ src/main/java/ru/inno/nalemian/controller/ObjectMapperController.java
@@ -1,11 +1,14 @@
 package ru.inno.nalemian.controller;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.DateFormat;
@@ -15,7 +18,8 @@ import java.util.Date;
 @RestController
 @RequestMapping("/object-mapper")
 public class ObjectMapperController {
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @PostMapping("/string-to-json")
     public JsonNode stringToJson(@RequestBody String jsonString) throws JsonProcessingException {
@@ -37,8 +41,26 @@ public class ObjectMapperController {
 
     @GetMapping("/java-to-json")
     public String javaObjectToJson() throws JsonProcessingException {
-        Car car = new Car("yellow", "renault");
+        Car car = new Car("yellow", "renault", new Date());
         return objectMapper.writeValueAsString(car);
+    }
+
+    @PostMapping("/json-to-java")
+    public Car jsonToJavaObject(@RequestBody String jsonString) throws JsonProcessingException {
+        return objectMapper.readValue(jsonString, Car.class);
+    }
+
+    @GetMapping("/get-dto-with-date")
+    public String getDtoWithDate() throws JsonProcessingException {
+        Car car = new Car("pink", "lamborgini", new Date());
+        return objectMapper.writeValueAsString(car);
+    }
+
+    @PostMapping("/post-dto-with-date")
+    public Car postDtoWithDate(@RequestBody Car car) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm a z");
+        System.out.println("Date: " + format.format(car.getDate()));
+        return car;
     }
 
     static class Car {
@@ -46,11 +68,17 @@ public class ObjectMapperController {
         private String color;
         @JsonProperty(value = "brand")
         private String type;
+        @Getter
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm a z")
+        private Date date;
 
-        public Car(String color, String type) {
+        public Car(String color, String type, Date date) {
             this.color = color;
             this.type = type;
+            this.date = date;
+        }
+
+        public Car() {
         }
     }
-
 }
