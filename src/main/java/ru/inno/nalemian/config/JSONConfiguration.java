f8af50ffa4ab3a76f9931f45fd95ec7548a53d32
nalemian <adelianagimova@gmail.com>
2025-05-12 17:26:59 +0500

    убрала ненужное

--- src/main/java/ru/inno/nalemian/config/JSONConfiguration.java
+++ src/main/java/ru/inno/nalemian/config/JSONConfiguration.java
@@ -2,14 +2,12 @@ package ru.inno.nalemian.config;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 
 @Configuration
-@RequiredArgsConstructor
 public class JSONConfiguration {
     @Bean
     public ObjectMapper objectMapper() {
